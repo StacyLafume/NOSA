a2635623bbfacc02b6b17f4433e877bb5eed52a1
Stacy Lafume <stacylafume123@gmail.com>
2023-06-29 21:42:16 -0400

    foundtion for pages

---
+++ src/pages/previousArtists.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PreviousArtists = () => {
+    return(
+        <div>
+            <h1>
+                PreviousArtists
+            </h1>
+        </div>
+    )
+}
+
+
+export default PreviousArtists;
---
+++ src/pages/previousEvents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PreviousEvents = () => {
+    return(
+        <div>
+            <h1>
+                PreviousEvents
+            </h1>
+        </div>
+    )
+}
+
+
+export default PreviousEvents;
